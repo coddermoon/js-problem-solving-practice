b134caa02dd3ef4dbfc14b3118824a8bf5c1668f
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-07-31 17:35:42 +0600

    test

---
+++ day1.js
@@ -0,0 +1,82 @@
+/**
+ *  Practice Problem 1
+ * 
+ * harry's mom  give money and asked him to buy some apples and orranges. write a program to help herry how much money shopkeeper return
+ * 
+ * 
+ * 
+ * 
+ */
+
+const momGiven = 10000;
+const applePrice = 400;
+const orrangesPrice = 300;
+
+
+const totalPrice = applePrice + orrangesPrice;
+
+// console.log('total Product price '+totalPrice + " Taka");
+
+const cashBack = momGiven - totalPrice;
+
+// console.log('shopkeeper cashbacked him '+ cashBack + ' taka');
+
+
+
+/**
+ * Prblem solving 2
+ * 
+ *  write a Program so that it can calculate aconstage mark of all subject
+ * 
+ */
+
+
+
+const bangla=78;
+const english=78;
+const math=78;
+const physics=78;
+const chemistry=78;
+const biology=78;
+const ict=68;
+
+
+const totalSubject = 7;
+
+
+const avarageMark = (bangla+ english+ math+physics + chemistry + biology + ict)/totalSubject;
+
+// console.log(avarageMark)
+
+
+
+/**
+ * Practice Problem 3
+ * 
+ * john's techer give him two variabe and print it only one line please write a program to print to variable string in one line and make only one sentence
+ * 
+ * 
+ */
+
+
+const variabe1 = `hello Programmer's`;
+const variabe2 =`How are you today?`;
+
+const sentence = variabe1+' ' + variabe2;
+
+// console.log(sentence)
+
+
+
+/**
+ * you should to know reminder in two divide number now write a programm
+ */
+
+const num1 = 31;
+const num2 = 5;
+const reminder = num1%num2
+
+console.log(reminder)
+
+
+
