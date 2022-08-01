e6d9d3c27bd16133414dfefd475b86640d17ece3
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-01 08:50:55 +0600

    problem 3 found

--- day2.js
+++ day2.js
@@ -74,3 +74,19 @@ fruits.push('waterlemon') //output is [ 'apple', 'Mango', 'waterlemon' ]
     }else{
         console.log('oh no! '+friendName+' '+ 'you are Failed. work hard and passed next chance')
     }
+
+    /**
+ * === === PROBLEM -3 === ===
+ * 
+*       1. You are given three numbers 13, 79, and 45. Write a program that will print the 
+        largest number using if-else.
+        2. You are given a triangle with the sides 9, 8, 9. Write a program to check whether a
+        triangle is Isosceles or not using if-else.
+        Isosceles => two sides are equal
+
+
+        
+
+
+ * 
+ */
