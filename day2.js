9cfa834b3b8c18b87e050622fcd5ec8196920e2e
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-01 08:44:14 +0600

    problem 2 solved in day 2

--- day2.js
+++ day2.js
@@ -35,4 +35,42 @@ fruits.pop() // ouput [ 'apple', 'Mango' ]
 
 fruits.push('waterlemon') //output is [ 'apple', 'Mango', 'waterlemon' ]
 
-console.log(fruits) 
+
+
+/**
+ * === === PROBLEM -2 === ===
+ * 
+ *  You and your friends Tom, Jane, Peter and John got their final exam results. Your total 
+    score is 85, Tom’s total score is 66, Jane’s total score is 95, Peter’s total score is 56
+    and John’s total score is 40. The grading chart is 
+    80 or above A grade
+    60 or above B grade
+    50 or above C grade
+    40 or above D grade
+    39 or less => F grade
+    Write a program to find your and your friends’ grades using if-else.
+
+        
+
+
+ * 
+ */
+
+    var friendName = 'tom';
+    var friendScore=59;
+
+    // check condition
+
+    if(friendScore>=80){
+        console.log('Dear '+friendName+' '+ 'you Got A Grade')
+    }else  if(friendScore>=60){
+        console.log('Dear '+friendName+' '+ 'you Got B grade')
+    
+    }else  if(friendScore>=50){
+        console.log('Dear '+friendName+' '+ 'you Got B grade')
+    
+    }else  if(friendScore>=40){
+        console.log('Dear '+friendName+' '+ 'you Got B grade')
+    }else{
+        console.log('oh no! '+friendName+' '+ 'you are Failed. work hard and passed next chance')
+    }
