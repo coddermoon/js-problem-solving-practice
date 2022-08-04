d313ffb96da94021de56d6014daf94850ecc83fa
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-04 12:23:45 +0600

    problem 3 solve done

--- day3.js
+++ day3.js
@@ -29,4 +29,41 @@
     }
 
     foo()
+
+
+    /**
+ * 
+ * ==========     === Practice Problem 2 === ==============
+ * 
+     2) Write a function called make_avg() which will take an three integers and return the
+     average of those values.
+
+ 
+ * 
+ */
+
+     function make_avg(num1,num2,num3) {
+
+        const average = (num1+num2+num3)/3;
+
+        return average;
+        
+     }
     
+     
+
+    //  call the function within 3 number of value
+
+    const subject1 = 60;
+    const subject2 = 59;
+    const subject3 = 60;
+
+   
+
+  var totalAvarage =   make_avg(subject1,subject2,subject3)
+
+    // console the output
+
+   console.log(totalAvarage);
+
+   
